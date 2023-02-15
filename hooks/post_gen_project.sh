61c343104c22d3c6753873d69b01a4b1037c86a4
Artem Dolya <artem.dolya@schibtsed.com>
2023-02-15 11:58:46 +0200

    Uncommented push to git

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -11,9 +11,9 @@ terraform apply --auto-approve
 mv backend_tf backend.tf
 terraform init -force-copy
 echo '**** Commiting code to repository: {{ cookiecutter.app_git_repo }})'
-# git init
-# git add .
-# git commit -m "initial commit"
-# git remote add origin {{ cookiecutter.app_git_repo }}
-# git push -u origin master
+git init
+git add .
+git commit -m "initial commit"
+git remote add origin {{ cookiecutter.app_git_repo }}
+git push -u origin master
 terraform output -json
