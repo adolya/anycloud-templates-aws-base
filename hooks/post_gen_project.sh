8b08961477ce30269c585df414091fda7300367d
Artem <adolya@users.noreply.github.com>
2023-02-16 12:55:16 +0200

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
+echo '**** Preparing Terraform **** '
 export AWS_REGION="{{ cookiecutter.AWS_REGION }}"
-echo "**** main region - $AWS_REGION"
 export AWS_ACCESS_KEY_ID="{{ cookiecutter.AWS_ACCESS_KEY_ID }}"
-echo "**** Using $AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="{{ cookiecutter.AWS_SECRET_ACCESS_KEY }}"
 terraform init
 terraform apply --auto-approve
 mv backend_tf backend.tf
 terraform init -force-copy
-echo '**** Commiting code to repository: {{ cookiecutter.app_git_repo }})'
+echo '**** Commiting code to repository: {{ cookiecutter.app_git_repo }}) ****'
 git init
 git add .
 git commit -m "initial commit"
