e9ca8192316a64df8450789ec08e8eaebc8283b6
Artem <adolya@users.noreply.github.com>
2023-02-16 12:54:10 +0200

    Update pre_gen_project.sh

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
 echo '**** Welcome to Cloud as Code Templates ****'
-echo '**** Terraform Version ****'
 terraform --version
 
-echo '**** Git Version ****'
-git --version
+
+set -u # or set -o nounset
+: "$AWS_REGION"
+: "$AWS_ACCESS_KEY_ID"
+: "$AWS_SECRET_ACCESS_KEY"
