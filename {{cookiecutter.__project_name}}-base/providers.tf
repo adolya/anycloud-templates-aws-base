dacc75c6b2b760e3a5e312b7f7841ea1a340c247
Artem Dolya <artem.dolya@gmail.com>
2023-03-03 21:30:02 +0200

    Switched to external dependecy

--- {{cookiecutter.__project_name}}-base/providers.tf
+++ {{cookiecutter.__project_name}}-base/providers.tf
@@ -1,18 +1,13 @@
-data "external" "terraform_version" {
-  program = ["bash", "${path.module}/scripts/get_terraform_version.sh"]
+module "utilities" {
+  source = "git::https://github.com/adolya/terraform-tools.git"
 }
-
-data "external" "timestamp" {
-  program = ["bash", "${path.module}/scripts/get_timestamp.sh"]
-}
-
 
 provider "aws" {
   default_tags {
     tags = merge({
-      Created          = data.external.timestamp.result.current
+      TerraformVersion = module.utilities.terraform_version
+      Created          = module.utilities.timestamp
       Project          = "{{cookiecutter.__project_name}}-base"
-      TerraformVersion = "v${data.external.terraform_version.result.version}"
       CaCT             = "Base"
       AppVersion       = var.APP_VERSION
       ManagedBy        = "Terraform"
